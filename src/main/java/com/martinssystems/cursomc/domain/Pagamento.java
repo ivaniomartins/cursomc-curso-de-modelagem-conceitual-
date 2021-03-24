e8e0e4254324e74ef4c79a2564a992760fd50e63
Ivanio Martins <ivaniomartins1991@gmail.com>
2021-03-24 19:15:48 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/martinssystems/cursomc/domain/Pagamento.java
+++ src/main/java/com/martinssystems/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.martinssystems.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -27,7 +27,7 @@ public abstract class Pagamento implements Serializable {
 	 * Mapeamento Bidirecional Um para Um (OneToOne) Para que o o id do pagamento
 	 * seja o mesmo id do pedido correpondente.
 	 */
-    @JsonBackReference
+    @JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
