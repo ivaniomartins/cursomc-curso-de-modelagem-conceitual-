f02e0c43491a3989662d75a3a6ff51a2bbf90d84
Ivanio Martins <ivaniomartins1991@gmail.com>
2021-04-07 22:00:02 -0300

    1. Deletando uma Categoria com DELETE; 2.Criando uma exceção personalizada.

--- src/main/java/com/martinssystems/cursomc/services/CategoriaService.java
+++ src/main/java/com/martinssystems/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.martinssystems.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.martinssystems.cursomc.domain.Categoria;
 import com.martinssystems.cursomc.repositories.CategoriaRepository;
+import com.martinssystems.cursomc.services.exceptions.DataIntegrityException;
 import com.martinssystems.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,14 +24,24 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
 	}
-	
+
 	public Categoria insert(Categoria obj) {
-		obj.setId(null);	
+		obj.setId(null);
 		return repo.save(obj);
 	}
-   
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+               throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
