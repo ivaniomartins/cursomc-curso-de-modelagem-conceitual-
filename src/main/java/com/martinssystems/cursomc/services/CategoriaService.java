e300fffc4a0224a630e3b4f429263ed58ea6ffb1
Ivanio Martins <ivaniomartins1991@gmail.com>
2021-04-11 16:49:34 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/martinssystems/cursomc/services/CategoriaService.java
+++ src/main/java/com/martinssystems/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.martinssystems.cursomc.domain.Categoria;
+import com.martinssystems.cursomc.dto.CategoriaDTO;
 import com.martinssystems.cursomc.repositories.CategoriaRepository;
 import com.martinssystems.cursomc.services.exceptions.DataIntegrityException;
 import com.martinssystems.cursomc.services.exceptions.ObjectNotFoundException;
@@ -60,4 +61,8 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		}
+	
+	public Categoria fromDto(CategoriaDTO objDTO) {
+		return new Categoria (objDTO.getId(), objDTO.getNome());
+	}
 }
