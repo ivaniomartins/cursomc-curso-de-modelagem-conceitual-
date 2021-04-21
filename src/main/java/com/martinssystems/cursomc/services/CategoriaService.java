fcee42b2281aef4fcd0df33ef4ce5f8ce4c4768f
Ivanio Martins <ivaniomartins1991@gmail.com>
2021-04-21 15:30:56 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/martinssystems/cursomc/services/CategoriaService.java
+++ src/main/java/com/martinssystems/cursomc/services/CategoriaService.java
@@ -36,8 +36,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -64,5 +65,12 @@ public class CategoriaService {
 	
 	public Categoria fromDto(CategoriaDTO objDTO) {
 		return new Categoria (objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
+		
+		
 	}
 }
