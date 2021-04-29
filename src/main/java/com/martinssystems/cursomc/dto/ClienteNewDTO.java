ecdd5801b77d98b738e4869d371e0921ece6b548
Ivanio Martins <ivaniomartins1991@gmail.com>
2021-04-29 17:14:07 -0300

    Validação customizada: CPF ou CNPJ na iserção

--- src/main/java/com/martinssystems/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/martinssystems/cursomc/dto/ClienteNewDTO.java
@@ -2,22 +2,49 @@ package com.martinssystems.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.martinssystems.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO  implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+    
+	@NotEmpty(message="Preenchimento deve ser obrigatório")
+	@Length(min=5, max =120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento deve ser obrigatório")
+	@Email(message="Email inválido")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento deve ser obrigatório")
+	
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento deve ser obrigatório")
 	private String logradouro;
+	
+	
+	@NotEmpty(message="Preenchimento deve ser obrigatório")
 	private String numero;
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento deve ser obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento deve ser obrigatório")
     private String telefone1;
-    private String telefone2;
+  
+	private String telefone2;
     private String telefone3;
     
     private Integer cidadeId;
